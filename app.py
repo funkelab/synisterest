203704479c772f8e76a47a16a88dfdf4d34490b3
nilsec <nils.eckstein@googlemail.com>
2021-06-08 07:26:53 -0400

    Update to use synistereq

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 import os
 import hashlib
+import redis
+from rq import Queue
 from flask import Flask, jsonify, request, redirect, url_for, render_template, flash, Markup
-from flask_restful import Api, Resource
-from model import get_neurotransmitter_predictions
 from werkzeug.utils import secure_filename
+from subprocess import check_call
 import secrets
 
+from model import predict
+
 app = Flask(__name__)
-api = Api(app)
 secret = secrets.token_urlsafe(32)
 app.secret_key = secret
+r = redis.Redis()
+q = Queue(connection=r)
 
 # Limit File size to 2GB
 app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 2 * 10**3
@@ -44,9 +48,29 @@ def parse_form():
         return redirect(url_for('index'))
     else:
         flash(Markup(render_template("redirect_uploading.html")), category="info")
-        uploaded_file.save(os.path.join("requests", secure_filename(f"{mail}.csv")))
+        job = gen_job(mail, uploaded_file, service, predict_from)
+        queue_job(job)
         flash(Markup(render_template("redirect_queued.html")), category="success")
         return redirect(url_for('index'))
+
+def queue_job(job):
+    task = q.enqueue(predict, job)
+    print(f"Queued job {task.id} to {task.enqueued_at}")
+
+def get_requests():
+    rdir = "requests"
+    requests = [os.path.join(rdir, f) for f in os.listdir(rdir) if f.endswith(".csv")]
+    return requests
+
+def gen_job(mail, uploaded_file, service, predict_from):
+    requests = get_requests()
+    request_id = len(requests)
+    uid = get_user_id(mail)
+    uploaded_file.save(os.path.join("requests", f"r_{uid}_{request_id}.csv"))
+    request = {"uid": uid, "request_id": request_id, 
+               "mail": mail, "service": service,
+               "predict_from": predict_from}
+    return request
 
 def has_access(user):
     registered_users = [l.strip() for l in open('users.txt', 'r').readlines()]
@@ -54,6 +78,12 @@ def has_access(user):
     if user_hash in registered_users:
         return True
     return False
+
+def get_user_id(user):
+    registered_users = [l.strip() for l in open('users.txt', 'r').readlines()]
+    user_hash = hashlib.sha512(str.encode(user)).hexdigest()
+    uid = registered_users.index(user_hash)
+    return uid
 
 def is_valid_option(service, predict_from):
     if not service in known_services:
@@ -64,67 +94,5 @@ def is_valid_option(service, predict_from):
         return predict_from == "POSITION"
     return True
 
-def read_skids_csv(csv_path):
-    """
-    The csv needs to have one column called 'skid'
-    """
-    data = pandas.read_csv(csv_path)
-    skids = data["skid"].to_list()
-    skids = list(set([int(skid) for skid in skids]))
-    return skids
-
-def read_positions_csv(csv_path):
-    """
-    The csv needs to have three columns called "id", "skid", "x", "y", "z"
-    """
-    data = pandas.read_csv(csv_path)
-    x_list = data["x"].to_list()
-    y_list = data["y"].to_list()
-    z_list = data["z"].to_list()
-    position_ids = data["id"].to_list()
-    skids = data["skid"].to_list()
-    position_ids_to_skids = {id_: skid for id_, skid in zip(position_ids, skids)}
-    positions = [(z,y,x) for z,y,x in zip(z_list, y_list, x_list)]
-    return positions, position_ids, position_ids_to_skids
-
-class Predict(Resource):
-    @staticmethod
-    def post():
-        data = request.get_json()
-        skids = data['skids']
-        presynaptic_locations = data['presynaptic_locations']
-        synapse_ids = data['synapse_ids']
-        dset = data['dset']
-        service = data['service']
-        user = data['user']
-
-        if Predict.has_access(user):
-            predictions = get_neurotransmitter_predictions(skids, presynaptic_locations, service, dset)
-
-            return jsonify({
-                'Input': {
-                    'skids': skids,
-                    'presynaptic_locations': presynaptic_locations,
-                    'dset': dset,
-                    'service': service,
-                    'user': user
-                },
-                'Predictions': predictions
-            })
-
-        else:
-            return "No access, Request access here"
-
-    @staticmethod
-    def has_access(user):
-        registered_users = [l.strip() for l in open('users.txt', 'r').readlines()]
-        user_hash = hashlib.sha512(str.encode(user)).hexdigest()
-        if user_hash in registered_users:
-            return True
-        return False
-
-
-api.add_resource(Predict, '/predict')
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5050)
--- model.py
+++ model.py
@@ -1,126 +1,41 @@
-recognized_datasets = ["FAFB", "HEMIBRAIN"]
-recognized_platforms = ["CATMAID", "NEUPRINT", "FLYWIRE", None]
+from synistereq.predict_neurotransmitters import predict_neurotransmitters
+from synistereq.utils import read_skids_csv, read_positions_csv, format_predictions, write_predictions, log_config
+from synistereq.report import generate_report
+import os
 
-def get_neurotransmitter_predictions(skids, presynaptic_locations, platform, dset):
-    presynaptic_locations = get_presynaptic_locations(skids, presynaptic_locations, platform, dset)
-    predictions = get_neurotransmitter_predictions_from_locations(presynaptic_locations, dset)
-    return predictions
+service_to_dset = {"CATMAID": "FAFB",
+                   "NEUPRINT": "HEMI",
+                   "FLYWIRE": "FAFB",
+                   "FAFB": "FAFB",
+                   "HEMI": "HEMI"}
 
-def get_neurotransmitter_predictions_from_locations(presynaptic_locations, dset):
-    dset_path = get_dset_path(dset)
-    predictions = [{"id": 0, "x": 0, "y": 1, "z": 2, "prediction": {"0": 0.1, "1": 0.9}}]
-    return predictions
+def predict(job):
+    uid = job["uid"]
+    request_id = job["request_id"]
+    mail = job["mail"]
+    service = job["service"]
+    predict_from = job["predict_from"]
 
-def get_presynaptic_locations(skids, presynaptic_locations, platform, dset):
-    """
-    skids: list of ints
-    presynaptic_locations: list of tuple of ints (x,y,z)
-    platform: string
-    dset: string
+    csv_file = f"requests/r_{uid}_{request_id}.csv"
+    position_ids_to_skids = {}
+    positions = None
+    position_ids = None
+    skids = None
+    if predict_from == "POSITION":
+        positions, position_ids, position_ids_to_skids = read_positions_csv(csv_file)
+    elif predict_from == "NEURON":
+        skids = read_skids_csv(csv_file)
+    else:
+        raise ValueError(f"Cannot predict from {predict_from}")
 
-    returns: a list of locations in the given dset
-    """
-    if not isinstance(skids, list):
-        raise TypeError("skids must be list type")
-    if not isinstance(presynaptic_locations, list):
-        raise TypeError("presynaptic locations must be list type")
-    if not isinstance(platform, str):
-        raise TypeError("platform must be string type")
-    if not isinstance(dset, str):
-        raise TypeError("dset must be string type")
+    dset = service_to_dset[service]
+    nt_probabilities, positions, \
+        position_ids, position_ids_to_skids = predict_neurotransmitters(dset,
+                                                                        service,
+                                                                        skids,
+                                                                        positions,
+                                                                        position_ids,
+                                                                        position_ids_to_skids,
+                                                                        batch_size=8)
 
-    skids = list(skids)
-    presynaptic_locations = list(presynaptic_locations)
-
-    get_presynaptic_locations_from_skid, transform_location = get_dset_platform_functions(dset, platform)
-    
-    if skids:
-        for skid in skids:
-            presynaptic_locations += get_presynaptic_locations_from_skid(skid)
-
-    presynaptic_locations = [transform_location(*loc) for loc in presynaptic_locations]
-
-    return presynaptic_locations
-
-def get_dset_platform_functions(dset, platform):
-    """
-    Returns the pair (a,b) where a is a function 
-    to query presynaptic locations from skid 
-    from the given (platform, dset) tuple and
-    b is a transformation of platform location 
-    """
-    if platform is None:
-        # Coordinates given in dataset space - no queries available, np transform required.
-        platform = dset
-
-    function_dict = register_dset_platform_functions()
-    try:
-        return function_dict[(dset, platform)]
-    except KeyError:
-        raise ValueError(f"Combination of platform {platform} and dataset {dset} not understood")
-
-def register_dset_platform_functions():
-    function_dict = {("FAFB", "CATMAID"): (get_fafb_catmaid_presynaptic_locations_from_skid, transform_fafb_catmaid_location),
-                     ("FAFB", "FLYWIRE"): (get_fafb_flywire_presynaptic_locations_from_skid, transform_fafb_flywire_location),
-                     ("HEMI", "NEUPRINT"): (get_hemi_neuprint_presynaptic_locations_from_skid, transform_hemi_neuprint_location),
-                     ("FAFB", "FAFB"): (get_fafb_fafb_presynaptic_locations_from_skid, transform_fafb_fafb_location),
-                     ("HEMI", "HEMI"): (get_hemi_hemi_presynaptic_locations_from_skid, transform_hemi_hemi_location)}
-
-    return function_dict
-
-def get_dset_path(dset):
-    return 0
-
-def get_fafb_catmaid_presynaptic_locations_from_skid(skid):
-    """
-    Returns a list [(x,y,z), ...] of catmaid locations for the given skid.
-    """
-    return [(0,1,2), (3,4,5)]
-
-def get_fafb_flywire_presynaptic_locations_from_skid(skid):
-    """
-    Returns a list [(x,y,z), ...] of flywire locations for the given skid,
-    """
-    return [(0,1,2), (3,4,5)]
-
-def get_hemi_neuprint_presynaptic_locations_from_skid(skid):
-    """
-    Returns a list [(x,y,z), ...] of neuprint locations for the given skid.
-    """
-    return [(0,1,2), (3,4,5)]
-
-def get_fafb_fafb_presynaptic_locations_from_skid(skid):
-    raise ValueError("Cannot query from skid without platform")
-
-def get_hemi_hemi_presynaptic_locations_from_skid(skid):
-    raise ValueError("Cannot query from skid without platform")
-
-def transform_fafb_catmaid_location(x, y, z):
-    """
-    Transforms catmaid coordinates to fafb_v14 space
-    """
-    return (x,y,z)
-
-def transform_hemi_neuprint_location(x, y, z):
-    """
-    Transforms neuprint coordinates to n5 hemi space
-    """
-    return (x,y,z)
-
-def transform_fafb_flywire_location(x, y, z):
-    """
-    Transforms fly wire coordinates to fafb_v14 space.
-    """
-    return (x,y,z)
-
-def transform_fafb_fafb_location(x,y,z):
-    """
-    Identity function if using FAFB space directly.
-    """
-    return (x,y,z)
-
-def transform_hemi_hemi_location(x,y,z):
-    """
-    Identity function if using hemibrain space directly
-    """
-    return (x,y,z)
+    print(nt_probabilities)
